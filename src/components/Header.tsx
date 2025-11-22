69f17fb63341eafed9358b4c61d39a0f10d545bb
techieshruti <shrutichandra721@gmail.com>
2025-11-22 20:28:10 +0530

    added header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,31 +1,53 @@
 import logo from "../assets/logo.png";
-import { ChevronDown, Search, BadgePercent, Headset,User,CookingPot } from "lucide-react";
+import {
+  ChevronDown,
+  Search,
+  BadgePercent,
+  Headset,
+  User,
+  CookingPot,
+} from "lucide-react";
+
+const navItems = [
+  { label: "Search", icon: Search },
+  { label: "Offers", icon: BadgePercent },
+  { label: "Help", icon: Headset },
+  { label: "Sign In", icon: User },
+  { label: "Cart", icon: CookingPot },
+];
+
 const Header = () => {
   return (
-    <div className="text-lg font-medium">
-      <div className="bg-[#f7e9df] w-full flex items-center justify-between py-3 px-20">
+    <div className="bg-[#f7e9df] text-lg font-medium">
+      <div className="max-w-7xl mx-auto flex items-center justify-between py-3 px-20">
+        
+        {/* Left Section*/}
+        <div className="flex gap-10">
+          {/* Logo */}
+          <img className="w-14 rounded-2xl" src={logo} alt="Grabit Logo" />
 
-        {/* left */}
-        <div className="flex justify-center items-center gap-10">
-          {/* logo */}
-          <img className="w-15 rounded-2xl" src={logo} alt="Grabit Logo" />
-          {/* location */}
-          <div className="underline flex">
-            <h1>Other</h1>
-            <ChevronDown />
+          {/* Location */}
+          <div className="flex items-center gap-1 underline cursor-pointer hover:text-[#f27318]">
+            <span>Other</span>
+            <ChevronDown size={18} />
           </div>
         </div>
 
-        {/* right */}
-        <div>
-          <ul className="flex gap-12">
-            <li className="flex gap-2 cursor-pointer hover:text-amber-800"><Search/>Search</li>
-            <li className="flex gap-2 cursor-pointer hover:text-amber-800"><BadgePercent/>Offers</li>
-            <li className="flex gap-2 cursor-pointer hover:text-amber-800"><Headset />Help</li>
-            <li className="flex gap-2 cursor-pointer hover:text-amber-800"><User/>Sign In</li>
-            <li className="flex gap-2 cursor-pointer hover:text-amber-800"><CookingPot/>Cart</li>
-          </ul>
-        </div>
+        {/* right section */}
+        <ul className="flex gap-12">
+          {navItems.map((item, index) => {
+            let Icon = item.icon;
+            return (
+              <li
+                key={index}
+                className="flex items-center gap-2 cursor-pointer hover:text-[#f27318] transition-colors"
+              >
+                <Icon size={20} />
+                {item.label}
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </div>
   );
