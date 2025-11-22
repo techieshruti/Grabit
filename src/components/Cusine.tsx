66672f68a066dfdb1329fe60b8dd044faff55afd
techieshruti <shrutichandra721@gmail.com>
2025-11-22 17:21:22 +0530

    added cusine sections

--- src/components/Cusine.tsx
+++ src/components/Cusine.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import northindian from "../assets/northindian.png"
+import burger from "../assets/burger.png"
+import pizza from "../assets/pizza.png"
 
 const Cusine = () => {
   return (
@@ -6,15 +8,31 @@ const Cusine = () => {
         <div className='w-3/4 bg-amber-600 '>
             <h1>What's on your mind?</h1>
             <div>
-                <ul>
+                <ul className="flex gap-4 justify-center items-center">
                     <li>
-                        <img src="" alt="" />
+                        <img className="w-20 rounded-full" src={northindian} alt="" />
                         <p>North Indian</p>
                     </li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
+                    <li>
+                        <img className="w-20 rounded-full" src={burger} alt="" />
+                        <p>Burgers</p>
+                    </li>
+                    <li>
+                        <img className="w-20 rounded-full" src={pizza} alt="" />
+                        <p>Pizzas</p>
+                    </li>
+                    <li>
+                        <img className="w-20 rounded-full" src={pizza} alt="" />
+                        <p>Pizzas</p>
+                    </li>
+                    <li>
+                        <img className="w-20 rounded-full" src={pizza} alt="" />
+                        <p>Pizzas</p>
+                    </li>
+                    <li>
+                        <img className="w-20 rounded-full" src={pizza} alt="" />
+                        <p>Pizzas</p>
+                    </li>
                 </ul>
             </div>
         </div>
