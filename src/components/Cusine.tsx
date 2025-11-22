98563b342325a05ed2b71decc45eb657d5b1270a
techieshruti <shrutichandra721@gmail.com>
2025-11-22 17:36:32 +0530

    added cusine sections

--- src/components/Cusine.tsx
+++ src/components/Cusine.tsx
@@ -4,33 +4,34 @@ import pizza from "../assets/pizza.png"
 
 const Cusine = () => {
   return (
-    <div className='flex'>
-        <div className='w-3/4 bg-amber-600 '>
-            <h1>What's on your mind?</h1>
-            <div>
-                <ul className="flex gap-4 justify-center items-center">
+    <div className='flex justify-center '>
+        <div className='w-3/4 px-4 py-4 border-b-4 border-b-gray-400'>
+            <h1 className="font-bold text-xl py-2">What's on your mind?</h1>
+
+            <div className="px-2 py-6">
+                <ul className="flex gap-6 items-center text-xl text-gray-600">
                     <li>
-                        <img className="w-20 rounded-full" src={northindian} alt="" />
+                        <img className="w-30 rounded-full" src={northindian} alt="" />
                         <p>North Indian</p>
                     </li>
                     <li>
-                        <img className="w-20 rounded-full" src={burger} alt="" />
+                        <img className="w-30 rounded-full" src={burger} alt="" />
                         <p>Burgers</p>
                     </li>
                     <li>
-                        <img className="w-20 rounded-full" src={pizza} alt="" />
+                        <img className="w-30 rounded-full" src={pizza} alt="" />
                         <p>Pizzas</p>
                     </li>
                     <li>
-                        <img className="w-20 rounded-full" src={pizza} alt="" />
+                        <img className="w-30 rounded-full" src={pizza} alt="" />
                         <p>Pizzas</p>
                     </li>
                     <li>
-                        <img className="w-20 rounded-full" src={pizza} alt="" />
+                        <img className="w-30 rounded-full" src={pizza} alt="" />
                         <p>Pizzas</p>
                     </li>
                     <li>
-                        <img className="w-20 rounded-full" src={pizza} alt="" />
+                        <img className="w-30 rounded-full" src={pizza} alt="" />
                         <p>Pizzas</p>
                     </li>
                 </ul>
