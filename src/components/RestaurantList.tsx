f09468f0a6376581eca3ab45f283b381f73916ef
techieshruti <shrutichandra721@gmail.com>
2025-11-24 17:30:25 +0530

    added RestaurantList section

--- src/components/RestaurantList.tsx
+++ src/components/RestaurantList.tsx
@@ -6,7 +6,7 @@ const RestaurantList = () => {
         <div className='w-3/4 bg-amber-700'>
             <div className='w-60 h-70 bg-blue-300 rounded-2xl'>
                 <img className='bg-amber-300 w-full h-1/2 rounded-2xl' src="image" alt="img" />
-                <h1 className='bg-gray-500'>title</h1>
+                <h1 className='bg-gray-500 text-xl font-bold py-2'>title</h1>
                 <p>rating</p>
                 <p>cusines</p>
                 <p>places</p>
