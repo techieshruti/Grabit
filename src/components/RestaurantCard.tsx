2537f1f7874666b03db80a57c585499523d7994d
techieshruti <shrutichandra721@gmail.com>
2025-11-25 16:29:41 +0530

    added RestaurantCard

--- src/components/RestaurantCard.tsx
+++ src/components/RestaurantCard.tsx
@@ -11,11 +11,11 @@ type RestaurantCardProps ={
 const RestaurantCard = ({image, title, rating, cuisine, area}: RestaurantCardProps) => {
   return (
     <section className="flex justify-center">
-      <div className="w-3/4 bg-amber-700">
-        <div className="w-60 bg-blue-300 rounded-2xl overflow-hidden shadow-md">
+      <div className="w-3/4">
+        <div className="w-60 bg-gray-100 rounded-2xl overflow-hidden shadow-md lg:w-55">
 
           {/* restaurant-image */}
-          <div className="bg-amber-300 w-full h-40">
+          <div className="w-full h-40">
             <img 
             className="w-full h-full object-cover" src={image} 
             alt={title} />
@@ -23,7 +23,7 @@ const RestaurantCard = ({image, title, rating, cuisine, area}: RestaurantCardPro
 
           {/* restaurant-info */}
           <div className="p-3">
-            <h1 className="bg-gray-500 text-lg font-bold">{title}</h1>
+            <h1 className="text-lg font-bold">{title}</h1>
 
             {/* restaurant rating info */}
             <div className="flex items-center gap-2">
