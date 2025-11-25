99d0b52cbaa7649a2142102d035be6c6ddf78832
techieshruti <shrutichandra721@gmail.com>
2025-11-25 13:24:28 +0530

    added RestaurantList section

--- src/components/RestaurantList.tsx
+++ src/components/RestaurantList.tsx
@@ -5,11 +5,17 @@ const RestaurantList = () => {
     <section className='flex justify-center'>
         <div className='w-3/4 bg-amber-700'>
             <div className='w-60 h-70 bg-blue-300 rounded-2xl'>
-                <img className='bg-amber-300 w-full h-1/2 rounded-2xl' src="image" alt="img" />
-                <h1 className='bg-gray-500 text-xl font-bold py-2'>title</h1>
+              {/* image */}
+              <div className='bg-amber-300 w-full h-1/2'>
+                <img src="image" alt="img" />
+              </div>
+              {/* details */}
+                <div>
+                  <h1 className='bg-gray-500 text-xl font-bold py-2'>title</h1>
                 <p>rating</p>
                 <p>cusines</p>
                 <p>places</p>
+                </div>
             </div>
         </div>
     </section>
