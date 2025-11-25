13c148a4bfe4a369bd0684947989826fa8e273b5
techieshruti <shrutichandra721@gmail.com>
2025-11-25 16:57:15 +0530

    added RestaurantCard

--- src/components/RestaurantCard.tsx
+++ src/components/RestaurantCard.tsx
@@ -10,36 +10,43 @@ type RestaurantCardProps ={
 
 const RestaurantCard = ({image, title, rating, cuisine, area}: RestaurantCardProps) => {
   return (
-    <section className="flex justify-center">
-      <div className="w-3/4">
-        <div className="w-60 bg-gray-100 rounded-2xl overflow-hidden shadow-md lg:w-55">
+    <div className="w-55 h-[320px] rounded-2xl overflow-hidden shadow-md bg-white hover:scale-[0.97] transition-all duration-300">
 
-          {/* restaurant-image */}
-          <div className="w-full h-40">
-            <img 
-            className="w-full h-full object-cover" src={image} 
-            alt={title} />
-          </div>
-
-          {/* restaurant-info */}
-          <div className="p-3">
-            <h1 className="text-lg font-bold">{title}</h1>
-
-            {/* restaurant rating info */}
-            <div className="flex items-center gap-2">
-              <Star size={16} fill="green" color="green" />
-              <span>{rating}</span>
-            </div>
-
-            {/* Cuisines */}
-            <p className="text-sm text-gray-600 truncate">{cuisine}</p>
-
-            {/* area */}
-            <p className="text-sm text-gray-500">{area}</p>
-          </div>
-        </div>
+      {/* Image Section */}
+      <div className="w-full h-40">
+        <img
+          className="w-full h-full object-cover"
+          src={image}
+          alt={title}
+        />
       </div>
-    </section>
+
+      {/* Info Section */}
+      <div className="p-3 flex flex-col justify-between h-[150px]">
+
+        {/* Top Info */}
+        <div>
+          <h1 className="text-xl font-bold line-clamp-2">
+            {title}
+          </h1>
+
+          {/* Rating */}
+          <div className="flex items-center gap-1 mt-1">
+            <Star size={14} fill="green" color="green" />
+            <span className="text-sm font-medium">{rating}</span>
+          </div>
+
+          {/* Cuisines */}
+          <p className="text-base text-gray-800 line-clamp-1 mt-1">
+            {cuisine}
+          </p>
+        </div>
+
+        {/* Bottom Area */}
+        <p className="text-base text-gray-800">{area}</p>
+
+      </div>
+    </div>
   );
 };
 
