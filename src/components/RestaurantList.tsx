a310237c1d56faa8730a72ad2a74eb17ce3c40f7
techieshruti <shrutichandra721@gmail.com>
2025-11-26 15:09:08 +0530

    added RestaurantCard

--- src/components/RestaurantList.tsx
+++ src/components/RestaurantList.tsx
@@ -1,4 +1,3 @@
-// components/RestaurantList.tsx
 import { useEffect, useState } from "react";
 import RestaurantCard from "./RestaurantCard";
 
@@ -7,6 +6,7 @@ const IMG_URL = "https://media-assets.swiggy.com/swiggy/image/upload/";
 const RestaurantList = () => {
 
   const [restaurants, setRestaurants] = useState<any[]>([]);
+  const [searchText, setSearchText] = useState("");
 
   useEffect(() => {
     fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.66770&lng=77.43370&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
@@ -15,17 +15,24 @@ const RestaurantList = () => {
 
         const cards = data?.data?.cards;
 
-        // Extract restaurant array safely
         const restaurantData =
           cards
             ?.map((card: any) => card?.card?.card?.gridElements?.infoWithStyle?.restaurants)
             .flat()
             .filter(Boolean) || [];
+            console.log("All Restaurants:", restaurantData);
 
         setRestaurants(restaurantData);
       })
       .catch((err) => console.error("API Error:", err));
   }, []);
+
+  // ✅ SEARCH FILTER LOGIC
+  const filteredRestaurants = restaurants.filter((res) =>
+    res.info.name.toLowerCase().includes(searchText.toLowerCase())
+  );
+  console.log("Filtered Restaurants:", filteredRestaurants); 
+console.log("Search text:", searchText);
 
   return (
     <section className="flex justify-center">
@@ -33,8 +40,21 @@ const RestaurantList = () => {
 
         <h1 className="text-2xl font-bold my-4">Restaurants Near You</h1>
 
+        {/* ✅ Search box */}
+        <div className="mb-6">
+          <input
+            type="text"
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+            placeholder="Search restaurants..."
+            className="border px-4 py-2 rounded-md w-80 outline-none focus:ring-2 focus:ring-[#f27318]"
+          />
+          
+        </div>
+
+        {/* ✅ Cards */}
         <div className="flex flex-wrap gap-6">
-          {restaurants.map((res) => (
+          {filteredRestaurants.map((res) => (
             <RestaurantCard
               key={res.info.id}
               image={IMG_URL + res.info.cloudinaryImageId}
